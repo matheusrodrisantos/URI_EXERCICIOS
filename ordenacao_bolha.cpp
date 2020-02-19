ef95fba18ea9844a7f2810af6e757282ab5c02a5
Maheus Rodrigues <matheus.rodrisantos@bol.com.br>
2020-02-19 14:52:12 -0300

    ORDENACAO_BOLHA

---
+++ ordenacao_bolha.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#define MAX 10
+using namespace std;
+
+class Carro
+{
+	public:
+		char nome[100];
+		char cor[20];
+		char placa[20];
+		double preco;
+};
+
+int main(int argc, char * argv[])
+{	
+	char resp; 
+	Carro carros[MAX];	
+	int i =0;
+	while(true)
+	{
+		cout<<"Digite o nome do carro: ";
+		cin>>carros[i].nome;
+		
+		cout<<"preco do carro\n";
+		cin>>carros[i].preco;
+                 			
+		cout <<"Deseja continuar ? <SIM> <NÃO> ";
+		cin>>resp;
+		if(resp!='S'){
+			break;
+		}
+		cout<<'\n';
+		i++;	
+	}	
+
+	for(int j=0;j<=i;j++)
+	{
+             cout<<"Nome do carro: "<<carros[j].nome;
+             cout<<"\nPreço: R$ "<<carros[j].preco;
+	     cout<<"\n";      
+                
+	}
+	return 0;
+}
